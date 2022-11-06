34e60e36ea8dc1fcbebf199847f38eb03c92b9b5
yvonne <yvonnewong1107@gmail.com>
2022-11-06 14:44:03 +0000

    updated and works

--- dice_rolling_simulator.py
+++ dice_rolling_simulator.py
@@ -6,28 +6,26 @@ import random
 #set min of 1 and max of 6
 #function that randomly gets a number between 1 and 6 and prints it
 
+#generate random number 
+#if random number within min and max, print
+#else, generate again
+#print the number
+
 def dice_roll():
-    min=1
-    max=6
+    min = 1
+    max = 6
     no_of_sides = random.randint(min, max)
-    #while 
     roll_gen = input("Do you want to roll the die? Y or N? ")
     if roll_gen == 'Y':
         print("You rolled a", no_of_sides)
-        #generate random number 
-        #if random number within min and max, print
-        #else, generate again
-        #print the number
         roll2 = input("Do you want to roll again? Y or N? ")
         while roll2 == 'Y':
             print("You rolled a", no_of_sides)
-            break
-            print(roll2) 
-        print("thanks")
-    elif roll_gen == 'N':
+            return dice_roll() #loop through and go back to roll_gen
         print("Thanks for playing")
     else:
-        print("Invalid answer")
-        #loop through and go back to roll_gen
+        print("Thanks for playing")
+
 
 dice_roll()
+
